d1db33ccb08d1882157a9c26efea34ae6810d49b
Edward Lee <edwardlee.259@gmail.com>
2020-10-17 16:10:54 -0700

    let's hope this works

---
+++ cpae/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# source activate tmp_cpae
+export PYTHONPATH=$PWD:$PYTHONPATH
+export FUEL_DATA_PATH=$PWD"/data/en_wn_full"
+export THEANO_FLAGS="optimizer=fast_run,device=cuda0,floatX=float32,gpuarray.preallocate=1,allow_gc=False"
+# for file in `find /home/edwlee/Documents/stanford/cs230/stanford-corenlp-4.1.0 -name "*.jar"`; do export
+#     CLASSPATH="$CLASSPATH:`realpath $file`";
+# done
+
+RESULTS_DIR="results/en_wn_full"
+# CONFIG_NAME="s2sg_w2v_defs_1_pen128"
+# CONFIG_NAME="s2sg_enwiki_full_1_pen0"
+CONFIG_NAME="s2sg_c300_pen8_c1_F"
+
+mkdir -p $RESULTS_DIR
+
+python bin/train_s2s.py --fast-start $CONFIG_NAME $RESULTS_DIR/$CONFIG_NAME
